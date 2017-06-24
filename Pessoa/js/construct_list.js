4b23cec3575b0d232c87b3c3a4b8ef9e3418174b
josuellions <josuel_lions@hotmail.com>
2017-06-24 01:13:05 -0300

    acesso server amazon prof

--- Pessoa/js/construct_list.js
+++ Pessoa/js/construct_list.js
@@ -11,9 +11,21 @@ $("#listar").click(function(){
             "postman-token": "78821fb2-d3bc-2800-bf0f-ccb2c893b975"
           }
         }
+       var ip = "http://54.233.161.95:4000/pessoa" ;;
+
+
+                   //$.each(exib, function(id,value){
+                    $('#table_02 > tbody').append('<tr>' + '<td>' + ip + '</td>' + '</tr>'
+                        );
+
+  
 
           $.ajax(settings).done(function (response) {
               var exib = response;
+
+
+
+
               $.each(exib, function(id,value){
                   if(exib.length )     { 
                       $('#table_01 > tbody').append('<tr>' + '<td>' + this.id + '</td>' + '<td>' + this.nome + 
@@ -21,6 +33,7 @@ $("#listar").click(function(){
                         );
                 //console.log(id,value)
                  }
+
            });
       });
 });
