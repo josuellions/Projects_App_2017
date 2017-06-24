860c8cf7039d76498473eaec37010a03c9dd5830
josuellions <josuel_lions@hotmail.com>
2017-06-24 01:28:57 -0300

    acesso server amazon prof

--- Pessoa/js/construct_list.js
+++ Pessoa/js/construct_list.js
@@ -3,7 +3,7 @@
 $("#listar").click(function(){
         var settings = {
           // "url": "http://21.21.21.5:3000/listar/pessoa",
-          "url":"http://54.233.161.95:4000/pessoa",
+          "url":"http://54.233.161.95:4000/listar/pessoa",
           "method": "GET",
           "headers": {
             "content-type": "application/x-www-form-urlencoded",
@@ -11,27 +11,14 @@ $("#listar").click(function(){
             "postman-token": "78821fb2-d3bc-2800-bf0f-ccb2c893b975"
           }
         }
-       var ip = "http://54.233.161.95:4000/pessoa" ;;
-
-
-                   //$.each(exib, function(id,value){
-                    $('#table_02 > tbody').append('<tr>' + '<td>' + ip + '</td>' + '</tr>'
-                        );
-
-  
 
           $.ajax(settings).done(function (response) {
               var exib = response;
-
-
-
-
               $.each(exib, function(id,value){
                   if(exib.length )     { 
                       $('#table_01 > tbody').append('<tr>' + '<td>' + this.id + '</td>' + '<td>' + this.nome + 
                         '</td>' + '</tr>'
                         );
-                //console.log(id,value)
                  }
 
            });
